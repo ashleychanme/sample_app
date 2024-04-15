65e3f7b43b55cdfec1d63f6077157136c9b72f0c
ashleychanme <zukikozuki24@gamil.com>
2024-04-15 12:29:28 +0900

    Add build script

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_10_131716) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_15_032332) do
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
